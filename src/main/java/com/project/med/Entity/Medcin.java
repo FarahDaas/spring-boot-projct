5227cb101dc907aa4e2602cad62df7734a537a84
FarahDaas <farahdaas@outlook.fr>
2022-11-09 17:28:43 +0100

    first commit

---
+++ src/main/java/com/project/med/Entity/Medcin.java
@@ -0,0 +1,14 @@
+package com.project.med.Entity;
+
+
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("medcin")
+public class Medcin  extends User{
+	
+	
+
+}
